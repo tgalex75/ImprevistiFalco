356262b756adc47ca316131fe6da196c7a7e7fda
tgalex75 <81436086+tgalex75@users.noreply.github.com>
2022-03-12 12:53:05 +0100

    v 1.3.3
    Footer Styled:
    
    - media-query rules

--- src/Credits.js
+++ src/Credits.js
@@ -4,6 +4,7 @@ export default function Credits(props) {
     return (
         <footer className="footer">
             <p className={props.mainState !== "welcome" ? 'black' : ''}>Coded by <span>tgalex75</span></p>
+            <p className={props.mainState !== "welcome" ? 'black' : ''}><span>Falconero</span> Community</p>
         </footer>
     )
 }
