69ebad1a21f0579fc300613466629c347ec13698
tgalex75 <81436086+tgalex75@users.noreply.github.com>
2022-09-29 10:38:04 +0200

    ErrorPage styled

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-  return (
-    <div>Page Not Found</div>
-  )
-}
+    const styles = {
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+        textAlign: "center",
+        zIndex: "10"
+    };
+    return (
+        <div style={styles}>
+            <h1>Page Not Found</h1>
+            <Link to="/home" style={{ fontSize: "1rem", color: "#ff0066", fontWeight: "600" }}>
+                <p>Torna alla Homepage</p>
+            </Link>
+        </div>
+    );
+};
 
-export default ErrorPage
+export default ErrorPage;
