69f556fb71b982d2c205f8533c568d98b5af4fcc
tgalex75 <81436086+tgalex75@users.noreply.github.com>
2023-01-06 16:04:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ export default function App() {
                 <AnimatedRoutes />
             </Router>
             <BgRotate />
-            //<DecorationsUp />
+            {/*<DecorationsUp /> */}
             <Footer />
         </>
     );
